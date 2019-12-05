aae70281ee3221c08536e4dcd801822e366a5e1c
Vitaly Yagufarov <for.viy@gmail.com>
2019-12-06 01:02:07 +0500

    Глупый бот.[#207247]

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+
+    public static String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            // заменить ... на правильный ответ rsl = "ответ по заданию".
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) { // заменить ... на проверку, известен ли боту этот вопрос и он знает как на него ответить.
+            // заменить ... на правильный ответ rsl = "ответ по заданию".
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
