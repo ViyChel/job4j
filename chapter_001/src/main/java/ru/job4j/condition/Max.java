224b296bb5d7c80392ea6f53119ad0b254c50d42
Vitaly Yagufarov <for.viy@gmail.com>
2019-12-06 16:19:33 +0500

    Максимум из двух чисел[#207251] Удалил Max.java MaxTest.java

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.condition;
-
-public class Max {
-    public static int max(int left, int right) {
-        boolean condition = left > right;
-        int result = condition ? left : right;
-        return result;
-    }
-}
