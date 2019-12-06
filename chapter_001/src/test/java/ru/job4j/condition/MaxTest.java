befbaf032af917c921b112bfec32d8deb650821d
Vitaly Yagufarov <for.viy@gmail.com>
2019-12-06 16:35:27 +0500

    Максимум из двух чисел[#207251]

--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -2,6 +2,7 @@ package ru.job4j.condition;
 
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class MaxTest {
@@ -10,4 +11,15 @@ public class MaxTest {
         int result = Max.max(1, 2);
         assertThat(result, is(2));
     }
+    @Test
+    public void whenMax1To2Then1() {
+        int result = Max.max(2, 1);
+        assertThat(result, is(2));
+    }
+    @Test
+    public void whenMax1To2Equals(){
+        int result = Max.max(3,3);
+        assertThat(result, is(3));
+    }
+
 }
