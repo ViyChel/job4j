ffe6313d42d3398330acce2efae7e3770e77735a
Vitaly Yagufarov <for.viy@gmail.com>
2019-12-06 16:15:21 +0500

    А существует ли треугольник?[#207254]

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -2,9 +2,8 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int result = left;
         boolean condition = left > right;
-        result = condition ? left : right;
+        int result = condition ? left : right;
         return result;
     }
 }
