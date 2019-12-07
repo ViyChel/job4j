76197b13436935f053746edc0935a261519ab100
Vitaly Yagufarov <for.viy@gmail.com>
2019-12-07 17:31:56 +0500

    Крест в псевдографике. [#207317]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Slash.java
@@ -0,0 +1,35 @@
+package ru.job4j.loop;
+
+/**
+ * Class Slash. Построение таблицы с индексами в консоли в виде креста.
+ * @author yagufarov
+ * @since 07.12.2019
+ * @version 1.0
+ */
+
+public class Slash {
+    public static void draw(int size) {
+        for (int row = 0; row < size; row++) {
+            for (int cell = 0; cell < size; cell++) {
+                boolean left = row == cell ? true : false; // добавить условие, по которому нужно определить ставить ли символ или нет.
+                boolean right = (cell == size - 1 - row) ? true : false; // добавить условие, что нужно ставить элемент в правый угол.
+                if (left) {
+                    System.out.print("0");
+                } else if (right) {
+                    System.out.print("0");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Draw by 3");
+        draw(3);
+        System.out.println("Draw by 5");
+        draw(5);
+    }
+}
