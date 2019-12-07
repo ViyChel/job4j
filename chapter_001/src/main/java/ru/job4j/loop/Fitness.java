4a154960981180c3fe7347f336cba5851d88164e
Vitaly Yagufarov <for.viy@gmail.com>
2019-12-07 11:47:56 +0500

    Протеиновая диета.[#207313]

--- chapter_001/src/main/java/ru/job4j/loop/Fitness.java
+++ chapter_001/src/main/java/ru/job4j/loop/Fitness.java
@@ -4,7 +4,7 @@ public class Fitness {
     public int calc(int ivan, int nik) {
         int month = 0;
 
-        while (!(ivan > nik)) {
+        while (ivan < nik) {
             ivan *= 3;
             nik *= 2;
             month++;
