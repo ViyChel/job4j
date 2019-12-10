f1182e83540bb02b6e80b949dc6cdddaa0346fa5
Vitaly Yagufarov <for.viy@gmail.com>
2019-12-10 16:57:57 +0500

    Поиск индекса в диапазоне.[#207264]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -1,12 +1,45 @@
 package ru.job4j.array;
 
+/**
+ * Class FindLoop. Содержит методы для поиска в массивах.
+ *
+ * @author Yagufarov
+ * @version 1.1
+ * @since 10.12.2019
+ */
+
 public class FindLoop {
+
+    /**
+     * Method indexOf. Осуществляет поиск элемента в массиве.
+     * @param data массив в котором делаем поиск
+     * @param el число которое ищем
+     * @return возвращаем индекс массива в котором найден элемент, либо -1 если элемент не найден.
+     */
     public int indexOf(int[] data, int el) {
         int rst = -1; // если элемента нет в массиве, то возвращаем -1.
         for (int index = 0; index < data.length; index++) {
             if (data[index] == el) {
-                rst = index;
-                break;
+                return index;
+            }
+        }
+        return rst;
+    }
+
+    /**
+     * Method indexOf. Осуществляет поиск в заданном диапазоне массива.
+     * @param data массив в котором делаем поиск
+     * @param el   число которое ищем
+     * @param start индекс массива с которого начинаем искать
+     * @param finish индекс массива в котором заканчиваем поиск
+     * @return возвращаем индекс массива в котором найден элемент, либо -1 если элемент не найден.
+     */
+
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        for (int index = start; index <= finish; index++) {
+            if (data[index] == el) {
+                return index;
             }
         }
         return rst;
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -26,4 +26,26 @@ public class FindLoopTest {
         int expect = -1;
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenNotFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 3;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
 }
