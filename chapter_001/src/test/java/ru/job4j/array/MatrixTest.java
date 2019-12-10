63240715a0d9a95487c73807af2e2e29bd6d6f3d
Vitaly Yagufarov <for.viy@gmail.com>
2019-12-10 21:26:09 +0500

    Двухмерный массив. Таблица умножения.[#207258]

---
+++ chapter_001/src/test/java/ru/job4j/array/MatrixTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MatrixTest {
+    @Test
+    public void when2on2() {
+        Matrix matrix = new Matrix();
+        int[][] table = matrix.multiple(2);
+        int[][] expect = {
+                {1, 2},
+                {2, 4}
+        };
+        assertThat(table, is(expect));
+    }
+
+    @Test
+    public void when5on5() {
+        Matrix matrix = new Matrix();
+        int[][] table = matrix.multiple(5);
+        int[][] expect = {
+                {1, 2, 3, 4, 5},
+                {2, 4, 6, 8, 10},
+                {3, 6, 9, 12, 15},
+                {4, 8, 12, 16, 20},
+                {5, 10, 15, 20, 25},
+        };
+        assertThat(table, is(expect));
+    }
+}
