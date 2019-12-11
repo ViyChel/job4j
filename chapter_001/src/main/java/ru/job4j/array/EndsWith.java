90a97e3ba57264b6a5b5acf02a7d845ad6bf6039
Vitaly Yagufarov <for.viy@gmail.com>
2019-12-11 09:05:04 +0500

    Слово заканчивается с ...[#207263]

--- chapter_001/src/main/java/ru/job4j/array/EndsWith.java
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -4,7 +4,7 @@ package ru.job4j.array;
  * Class EndsWith. Класс содержит метод, который проверяет, что слово заканчивается c определенной последовательностью.
  *
  * @author yagufarov
- * @version 1.0
+ * @version 1.1
  * @since 10.12.2019
  */
 
@@ -12,7 +12,7 @@ public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         // проверить. что массив word имеет последние элементы одинаковые с post
         for (int i = 0; i < post.length; i++) {
-            if (post[post.length - 1] != word[word.length - 1]) {
+            if (post[post.length - 1 - i] != word[word.length - 1 - i]) {
                 return false;
             }
         }
