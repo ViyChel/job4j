95d0000bd52e15cb3587bcbcd5269e5dd137a5f6
Vitaly Yagufarov <for.viy@gmail.com>
2019-12-13 13:58:48 +0500

    Объединить два массива [#124482]

---
+++ chapter_001/src/main/java/ru/job4j/sort/Merge.java
@@ -0,0 +1,60 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+/**
+ * Class Merge
+ * Test task is for the exam "Basic Syntax"
+ * @author Vitaly Yagufarov
+ * @version 1.0
+ * @since 13.12.2019
+ */
+
+public class Merge {
+
+    /**
+     * Method merge is designed to merge two sorted arrays into one with general sorting.
+     * @param left   first array int []
+     * @param right  second array int []
+     * @return int[] Sorted array after merging two arrays
+     */
+
+    public int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+        int indexLeft = 0;
+        int indexRight = 0;
+
+        while (indexLeft + indexRight != rsl.length) {
+            if (indexLeft < left.length && indexRight == right.length) {
+                rsl[indexLeft + indexRight] = left[indexLeft];
+                indexLeft++;
+            } else if (indexRight < right.length && indexLeft == left.length) {
+                rsl[indexLeft + indexRight] = right[indexRight];
+                indexRight++;
+            } else {
+                if (left[indexLeft] < right[indexRight]) {
+                    rsl[indexLeft + indexRight] = left[indexLeft];
+                    indexLeft++;
+                } else {
+                    rsl[indexLeft + indexRight] = right[indexRight];
+                    indexRight++;
+                }
+            }
+        }
+        return rsl;
+    }
+
+    /**
+     * Method main for testing the method merge {@link Merge#merge(int[], int[])}
+     * @param args The main method accepts a single argument: an array of elements of type String.
+     */
+
+    public static void main(String[] args) {
+        Merge process = new Merge();
+        int[] rsl = process.merge(
+                new int[]{1, 3, 5},
+                new int[]{2, 4}
+        );
+        System.out.println(Arrays.toString(rsl));
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/sort/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The package is for tasks using sorting.
+ *
+ * @author Vitaly Yagufarov (for.viy@gmail.com)
+ * @version 1.0
+ * @since 07.12.2019
+ */
+package ru.job4j.sort;
