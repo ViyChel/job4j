9297edbb2ac7fbbf19e8683165f868da43498bd0
Vitaly Yagufarov <for.viy@gmail.com>
2019-12-13 13:56:39 +0500

    add package-info.java to all packages

--- chapter_001/src/main/java/ru/job4j/array/package-info.java
+++ chapter_001/src/main/java/ru/job4j/array/package-info.java
@@ -1,8 +1,8 @@
 /**
- * The package for tasks using calculations.
+ * The package are solutions to tasks with arrays.
  *
  * @author Vitaly Yagufarov (for.viy@gmail.com)
  * @version 1.0
  * @since 07.12.2019
  */
-package ru.job4j.calculator;
+package ru.job4j.array;
--- chapter_001/src/main/java/ru/job4j/loop/package-info.java
+++ chapter_001/src/main/java/ru/job4j/loop/package-info.java
@@ -1,8 +1,8 @@
 /**
- * The package for tasks using calculations.
+ * The package for tasks using loops.
  *
  * @author Vitaly Yagufarov (for.viy@gmail.com)
  * @version 1.0
  * @since 07.12.2019
  */
-package ru.job4j.calculator;
+package ru.job4j.loop;
