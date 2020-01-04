37f17b63f1cd83f5715a4dd64f1e644b6362a707
Vitaly Yagufarov <for.viy@gmail.com>
2020-01-04 16:57:48 +0500

    2. Рефакторинг - Шаблон Декоратор для валидатора.[#207387]

--- chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -1,26 +1,52 @@
 package ru.job4j.tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class ValidateInputTest {
 
+    private ByteArrayOutputStream mem = new ByteArrayOutputStream();
+    private PrintStream out = System.out;
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.mem));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.out);
+    }
+
     @Test
-    public void whenInvalidInput() {
-        ByteArrayOutputStream mem = new ByteArrayOutputStream();
-        PrintStream out = System.out;
-        System.setOut(new PrintStream(mem));
+    public void whenInvalidKeyMenuInput() {
         ValidateInput input = new ValidateInput(
-                new StubInput(new String[] {"invalid", "1"})
+                new StubInput(new String[]{"-1", "0"})
         );
         input.askInt("Enter", 1);
         assertThat(
                 mem.toString(),
-                is(String.format("Please enter validate data again.%n"))
+                is(String.format("Please select key from menu %n"))
         );
-        System.setOut(out);
+    }
+
+    @Test
+    public void whenInvalidInput() {
+        ValidateInput input = new ValidateInput(
+                new StubInput(new String[]{"invalid", "1"})
+        );
+        input.askInt("Enter", 1);
+        assertThat(
+                mem.toString(),
+                is(String.format("Please enter validate data again %n"))
+        );
     }
 }
+
