791f6b5e86e58323a518955e01fceb7d925167f3
Vitaly Yagufarov <for.viy@gmail.com>
2020-01-21 01:20:51 +0500

    2. Идентичные тексты. [#212655]

---
+++ chapter_003/src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>(Arrays.asList(origin));
+        for (String s : text) {
+            if (!check.contains(s)) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
