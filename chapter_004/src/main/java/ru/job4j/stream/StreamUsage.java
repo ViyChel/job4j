4de9fc6c6dcba9c920698ff044d0e6060807ca67
Vitaly Yagufarov <for.viy@gmail.com>
2020-01-24 21:54:37 +0500

    0. Stream API[#207223]

---
+++ chapter_004/src/main/java/ru/job4j/stream/StreamUsage.java
@@ -0,0 +1,36 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamUsage {
+    public static class Task {
+        private final String name;
+        private final long spent;
+
+        public Task(String name, long spent) {
+            this.name = name;
+            this.spent = spent;
+        }
+
+        @Override
+        public String toString() {
+            return "Task{"
+                    + "name='" + name + '\''
+                    + ", spent=" + spent
+                    + '}';
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Task> tasks = List.of(
+                new Task("Bug #1", 100),
+                new Task("Task #2", 100),
+                new Task("Bug #3", 100)
+        );
+        List<Task> bugs = tasks.stream().filter(
+                task -> task.name.contains("Bug")
+        ).collect(Collectors.toList());
+        bugs.forEach(System.out::println);
+    }
+}
