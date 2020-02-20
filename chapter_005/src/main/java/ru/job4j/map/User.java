50b978f2efbddf616898c0d417f091f9d77e973a
Vitaly Yagufarov <for.viy@gmail.com>
2020-02-20 22:42:51 +0500

    3. Переопределить только hashCode[#224612]

--- chapter_005/src/main/java/ru/job4j/map/User.java
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,7 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.Objects;
 
 /**
  * Class User
@@ -9,40 +10,81 @@ import java.util.Calendar;
  * @version 1.0
  * @since 20.02.2020
  */
-
 public class User {
     private String name;
     private int children;
     private Calendar birthday;
 
+    /**
+     * Instantiates a new User.
+     *
+     * @param name     the name
+     * @param children the children
+     * @param birthday the birthday
+     */
     public User(String name, int children, Calendar birthday) {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
     }
 
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets children.
+     *
+     * @return the children
+     */
     public int getChildren() {
         return children;
     }
 
+    /**
+     * Sets children.
+     *
+     * @param children the children
+     */
     public void setChildren(int children) {
         this.children = children;
     }
 
+    /**
+     * Gets birthday.
+     *
+     * @return the birthday
+     */
     public Calendar getBirthday() {
         return birthday;
     }
 
+    /**
+     * Sets birthday.
+     *
+     * @param birthday the birthday
+     */
     public void setBirthday(Calendar birthday) {
         this.birthday = birthday;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
     }
 }
 
