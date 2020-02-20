d12cc90b1dcd93c41b5d5108d31b45fac094caf9
Vitaly Yagufarov <for.viy@gmail.com>
2020-02-21 00:08:35 +0500

    4. Переопределить только equals[#224613]

--- chapter_005/src/main/java/ru/job4j/map/User.java
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -83,8 +83,17 @@ public class User {
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(name, children, birthday);
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children
+                && Objects.equals(name, user.name)
+                && Objects.equals(birthday, user.birthday);
     }
 }
 
