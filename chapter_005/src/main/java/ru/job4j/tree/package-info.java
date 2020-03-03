676f0b223cb0c13ba54a807a27f187b8d7738f38
Vitaly Yagufarov <for.viy@gmail.com>
2020-03-03 23:17:23 +0500

    1. Создать элементарную структуру дерева[#224621]

---
+++ chapter_005/src/main/java/ru/job4j/tree/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The package are solutions to tasks with Tree interface.
+ *
+ * @author Vitaly Yagufarov (for.viy@gmail.com)
+ * @version 1.0
+ * @since 03.03.2020
+ */
+package ru.job4j.tree;
