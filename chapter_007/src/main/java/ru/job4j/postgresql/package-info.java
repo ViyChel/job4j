4c656c748a6ef2a032305ebff72877aea94b9a44
Vitaly Yagufarov <for.viy@gmail.com>
2020-04-02 19:09:12 +0500

    Change chapter_007

---
+++ chapter_007/src/main/java/ru/job4j/postgresql/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The package are solutions to tasks with postgresql.
+ *
+ * @author Vitaly Yagufarov (for.viy@gmail.com)
+ * @version 1.0
+ * @since 27.03.2020
+ */
+package ru.job4j.postgresql;
